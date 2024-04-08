65f716ed2aebd7b93fcf216962041f0dcbbf34e5
Ajeet Chaulagain <chaulagainajeet@gmail.com>
2024-04-08 19:05:21 +1000

    fix: try not to boot api server

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -5,5 +5,5 @@ test("shows correct username on landing page", async ({ page }) => {
 
   const user = page.getByTestId("user-copy");
 
-  expect(user).toContainText("Ajeet!!");
+  expect(user).toContainText("testUser");
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -70,9 +70,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: [
-    {
-      command: "npm run start:api-server",
-    },
+    // {
+    //   command: "npm run start:api-server",
+    // },
     {
       command: "npm run dev",
       reuseExistingServer: !process.env.CI,
